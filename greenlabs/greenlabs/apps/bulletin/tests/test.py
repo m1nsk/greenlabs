df354102eae6844ef75fb288fa26c79524562184
m1nsk <punch.korolev@yandex.ru>
2017-10-29 15:52:45 +0300

    lilfix

--- greenlabs/greenlabs/apps/bulletin/tests/test.py
+++ greenlabs/greenlabs/apps/bulletin/tests/test.py
@@ -2,6 +2,7 @@ from django.test import Client
 from django.test import TestCase
 from django.http import Http404
 from ..models import Client
+from ..services.account_service import user_is_creator, user_is_executor
 
 
 class AccordanceAnonymousTestCase(TestCase):
@@ -41,6 +42,12 @@ class AccordanceCreatorTestCase(TestCase):
     def setUp(self):
         c = Client()
         c.login(username='creator1', password='password123')
+
+    def test_user_is_creator_success(self):
+        self.assertEqual(user_is_creator(), True)
+
+    def test_user_is_executor_fail(self):
+        self.assertEqual(user_is_executor(), False)
 
     def test_order_list_success(self):
         c = Client()
